d923637191edc77fb9af7f2ef05f034667fc3508
Mimounnn <150055356+Mimounnn@users.noreply.github.com>
2024-01-09 17:30:12 +0000

    Added first Spring Boot Project

---
+++ hello/src/main/java/edu/fra/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package edu.fra.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return "<h1> Hello World Spring Boot! </h1>";
+    }
+}
